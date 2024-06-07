a274b268bfd2519e0bb354c8f3c6624703eb58a3
julien leblanc <julien2.leblanc@gmail.com>
2024-06-07 12:00:04 +0200

    fix

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -7,7 +7,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
-use LogicException;
 
 class LoginController extends AbstractController
 {
@@ -29,7 +28,7 @@ class LoginController extends AbstractController
     #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
     {
-        throw new LogicException('This method can be blank - 
+        throw new LogicException('This method can be blank -
         it will be intercepted by the logout key on your firewall.');
     }
 }
