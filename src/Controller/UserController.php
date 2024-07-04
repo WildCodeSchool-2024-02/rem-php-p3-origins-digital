c91487e92fed12a3193244e500f2173a5f1ac51c
Bourdier Gabriel <bourdier.gabriel@sfr.fr>
2024-07-04 16:06:34 +0200

    Back pour la configuration utilisateur crée

---
+++ src/Controller/UserController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class UserController extends AbstractController
+{
+    #[Route('/utilisateur/edition/{id}', name:'user')]
+    public function edit(User $user, EntityManagerInterface $entityManager, Request $request):Response
+    {
+        if(!$this->getUser()) {
+            return $this->redirectToRoute('app_login');
+        }
+
+        if(!$this->getUser() === $user) {
+            return $this->redirectToRoute('app_home');   
+        }
+        
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($user);
+            $entityManager->flush();
+            $this->addFlash('success', 'Votre profil a été mis à jour.');
+            return $this->redirectToRoute('user', [
+                'id' => $user->getId()
+            ]);
+        }
+        
+        return $this->render('user/useredit.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+}
