3e1a0ba037d1bd4b0f04b1bc6f21e6a8c3531a0d
julien leblanc <julien2.leblanc@gmail.com>
2024-07-05 09:48:32 +0200

    Theme + fixtuer + form added

---
+++ src/DataFixtures/ThemeFixtures.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Theme;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class ThemeFixtures extends Fixture
+{
+    const THEME = [
+        'Anime',
+        'Horror',
+        'Sci-Fi',
+        'Cyberpunk',
+        'Space',
+        'Survival',
+        'Open World',
+        'Coop',
+        'MMO',
+        'Multiplayer',
+        'Competitive',
+        'Single Player'
+
+    ];
+    public function load(ObjectManager $manager): void
+    {
+        foreach (self::THEME as $key => $theme) {
+            $newTheme = new Theme();
+            $newTheme->setName($theme);
+            $this->addReference('theme'.$key, $newTheme);
+            $manager->persist($newTheme);
+        }
+        $manager->flush();
+    }
+}
